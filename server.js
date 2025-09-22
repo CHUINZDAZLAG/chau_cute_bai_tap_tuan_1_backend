71164868a725ac06b3f984854a266119e6077c23
CHOWTHANKS <nguyenthithanhchau2004@gmail.com>
2025-09-22 12:31:08 +0700

    first

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+// Fake data
+let todos = [
+  { id: 1, task: "Học React", done: false },
+  { id: 2, task: "Học Node.js", done: true },
+];
+
+// API get all todos
+app.get("/api/todos", (req, res) => {
+  res.json(todos);
+});
+
+// API add new todo
+app.post("/api/todos", (req, res) => {
+  const newTodo = { id: todos.length + 1, task: req.body.task, done: false };
+  todos.push(newTodo);
+  res.json(newTodo);
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
